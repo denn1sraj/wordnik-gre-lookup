251b0e69deac2be03be7c853e6561daee7450021
denn1sraj <ucdkraj@gmail.com>
2024-12-24 22:13:38 -0500

    added error handling v1
    Signed-off-by: denn1sraj <ucdkraj@gmail.com>

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 ~~1. Read local CSV file~~ 
 ~~2. Call the API to look up words~~
 ~~3. Write to a CSV file~~
-4. add error handling with a log 
+~~4. add error handling with a log~~ 
+5. add fix for error handling within
 
 ## Gsheets Dev -- Future Feature
 
--- gre-lookup.py
+++ gre-lookup.py
@@ -10,6 +10,9 @@ with open("Key/wordnikapi.txt", "r") as file:
 # wordnik base url
 base_url = 'https://api.wordnik.com/v4/word.json/'
 
+# define a log file
+log_file = "error_log.txt"
+
 # import GRE words
 df = pd.read_csv("Assets/gre words.csv")
 
@@ -20,49 +23,58 @@ df = df.assign(part='',
                example = '')
 
 # testing limit 
-limit = 1
+limit = 30
 
-for row in df.itertuples():
-    # testing 
-    # if row.Index > limit:
-    #     break
-    print(f"Index: {row.Index}, Word: {row.Word}")
-    url_def = f"{base_url}{row.Word}/definitions?api_key={key}"
-    url_rel = f"{base_url}{row.Word}/relatedWords?api_key={key}"
-    url_topex = f"{base_url}{row.Word}/topExample?api_key={key}"
+with open("error_log.txt", "w") as log:
 
-    # call / store json for definition
-    response = urlopen(url_def)
-    json_data = json.loads(response.read())[0] ## pulls just the first response
-    # extract definition data
-    definition = json_data['text']
-    part = json_data['partOfSpeech']
+    for row in df.itertuples():
+        # testing 
+        if row.Index > limit:
+            break
+        try: 
 
-    # repeat call / store for synonym
-    response = urlopen(url_rel)
-    json_data = json.loads(response.read())
-    # extract synonym data
-    synonyms = next(item['words'] for item in json_data if item['relationshipType'] == 'synonym')
+            print(f"Starting Index: {row.Index}")
+            url_def = f"{base_url}{row.Word}/definitions?api_key={key}"
+            url_rel = f"{base_url}{row.Word}/relatedWords?api_key={key}"
+            url_topex = f"{base_url}{row.Word}/topExample?api_key={key}"
 
-    # repeat call / store for top example
-    response = urlopen(url_topex)
-    json_data = json.loads(response.read())
-    # extract example data
-    example = json_data['text']
+            # call / store json for definition
+            response = urlopen(url_def)
+            json_data = json.loads(response.read())[0] ## pulls just the first response
+            # extract definition data
+            definition = json_data['text']
+            part = json_data['partOfSpeech']
 
-    print(f"Updating: {row.Word}")
-    df.at[row.Index, 'part'] = part
-    df.at[row.Index, 'definition'] = definition
-    df.at[row.Index, 'synonyms'] = synonyms
-    df.at[row.Index, 'example'] = example
-    print(f"{row.Word} is updated with the part of speech, definition, synonyms and examples")
+            # repeat call / store for synonym
+            response = urlopen(url_rel)
+            json_data = json.loads(response.read())
+            # extract synonym data
+            synonyms = next(item['words'] for item in json_data if item['relationshipType'] == 'synonym')
 
+            # repeat call / store for top example
+            response = urlopen(url_topex)
+            json_data = json.loads(response.read())
+            # extract example data
+            example = json_data['text']
 
+            print(f"Updating: {row.Word}")
+            df.at[row.Index, 'part'] = part
+            df.at[row.Index, 'definition'] = definition
+            df.at[row.Index, 'synonyms'] = synonyms
+            df.at[row.Index, 'example'] = example
+            print(f"{row.Word} is updated with the part of speech, definition, synonyms and examples")
+            print(f"Index {row.Index} processed correctly")
 
-    headers = response.info()
-    rate_remaining = headers.get("X-RateLimit-Remaining-Minute")
-    print("Rate Remaining (Per Minute):", rate_remaining)
-    sleep(30)
+            # optional // check to see if you are running up against the rate limit 
+            headers = response.info()
+            rate_remaining = headers.get("X-RateLimit-Remaining-Minute")
+            print("Rate Remaining (Per Minute):", rate_remaining, "\n")
+            sleep(30)
+
+        except Exception as e:
+                log.write(f"error processing row {row.Index} - {e}\n")
+                print(f"Error processing row {row.Index} for {row.Word} \nMoving on to the next word. \n")
+                sleep(30)
 
 file_path = "Output/gre words updated.csv"
 
